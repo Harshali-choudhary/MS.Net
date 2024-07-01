0a617088013825cfcc755cf6f18b98e2a91aef30
Harshali-choudhary <harshalichoudhary27@gmail.com>
2024-07-01 22:05:38 +0530

    Console Application

---
+++ memMgmtApp/memMgmtApp/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memMgmtApp
+{
+    internal class Product: IDisposable
+    {
+
+            public string Title { get; set; }
+            public double unitPrice { get; set; }
+            public Product(String title, double unitPrice)
+            {
+                Title = title;
+                this.unitPrice = unitPrice;
+            }
+
+            ~Product() { }
+
+            public void Dispose()
+            {
+                GC.SuppressFinalize(this);
+            }
+        }
+}
---
+++ memMgmtApp/memMgmtApp/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using memMgmtApp;
+
+using (Product p1 = new Product("Gerbera", 12))
+{
+    Console.WriteLine("Product added successfully");
+}
+
